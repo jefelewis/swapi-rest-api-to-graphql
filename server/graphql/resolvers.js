b052b71911980d8ef1420fe7d4f366c67811fa5f
Jeff Lewis <jeffylew77@gmail.com>
2018-07-14 22:55:48 -0700

    [Dev] Adding resolvers for searching for a type by ID

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -5,12 +5,42 @@ import fetch from 'node-fetch';
 // GraphQL: Resolvers
 const RESOLVERS = {
   Query: {
-    // Search for a film by ID
+    // Search for a Film by ID
     getFilm: async (parent, args) => {
       const response = await 
       fetch(`https://swapi.co/api/films/${args.id}`);
       return response.json();
-    }
+    },
+    // Search for a Person by ID
+    getPerson: async (parent, args) => {
+      const response = await 
+      fetch(`https://swapi.co/api/people/${args.id}`);
+      return response.json();
+    },
+    // Search for a Planet by ID
+    getPlanet: async (parent, args) => {
+      const response = await 
+      fetch(`https://swapi.co/api/planets/${args.id}`);
+      return response.json();
+    },
+    // Search for a Species by ID
+    getSpecies: async (parent, args) => {
+      const response = await 
+      fetch(`https://swapi.co/api/species/${args.id}`);
+      return response.json();
+    },
+    // Search for a Starship by ID
+    getStarship: async (parent, args) => {
+      const response = await 
+      fetch(`https://swapi.co/api/starships/${args.id}`);
+      return response.json();
+    },
+    // Search for a Vehicle by ID
+    getVehicle: async (parent, args) => {
+      const response = await 
+      fetch(`https://swapi.co/api/vehicles/${args.id}`);
+      return response.json();
+    },
   }
 };
 
